aae2eecd7ff3dcf98bf17948ac1f2c7f5744555b
Ach Muqoddam <ach.adam04@gmail.com>
2024-07-16 09:30:01 +0700

    menentukan business logic

--- App.php
+++ App.php
@@ -1,5 +1,8 @@
 <?php 
 
 require_once "Model/Todolist.php";
+require_once "BusinessLogic/showTodoList.php";
+require_once "BusinessLogic/addTodoList.php";
+require_once "BusinessLogic/removeTodoList.php";
 
 echo "Aplikasi Todolist" . PHP_EOL;
