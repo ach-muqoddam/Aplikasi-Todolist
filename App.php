aefe482a2e2a4d8f17f0152afa247e4413b03392
Ach Muqoddam <ach.adam04@gmail.com>
2024-07-16 09:33:15 +0700

    menentukan view

--- App.php
+++ App.php
@@ -1,8 +1,13 @@
 <?php 
 
 require_once "Model/Todolist.php";
+
 require_once "BusinessLogic/showTodoList.php";
 require_once "BusinessLogic/addTodoList.php";
 require_once "BusinessLogic/removeTodoList.php";
+
+require_once "View/ViewShowTodoList.php";
+require_once "View/ViewAddShowTodoList.php";
+require_once "View/ViewRemoveTodoList.php";
 
 echo "Aplikasi Todolist" . PHP_EOL;
