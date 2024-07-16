7069340ec7703f27f19d165eaa83cff38392e3a8
Ach Muqoddam <ach.adam04@gmail.com>
2024-07-16 10:18:51 +0700

    menambah todolist

--- BusinessLogic/addTodoList.php
+++ BusinessLogic/addTodoList.php
@@ -4,9 +4,13 @@
 menambahkan todo di list
 */
 
-function addTodoList()
+function addTodoList(string $todo)
 {
-    
+    global $todoList; // global scope
+
+    $number = sizeof($todoList) + 1;
+
+    $todoList[$number] = $todo;
 }
 
 
