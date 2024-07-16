c07987fc6eca87dd4db7840078322af3fca70576
Ach Muqoddam <ach.adam04@gmail.com>
2024-07-16 13:09:03 +0700

    menghapus todolist

--- BusinessLogic/removeTodoList.php
+++ BusinessLogic/removeTodoList.php
@@ -4,9 +4,24 @@
 menghapus todo di list
 */
 
-function removeTodoList()
+function removeTodoList(int $number): bool
 {
+    global $todoList; // global scope
 
+    // logic cek data true atau false
+    if ($number > sizeof($todoList)){
+        return false;
+    }
+
+    // logic menggeser data
+    for ($i = $number; $i < sizeof($todoList); $i++) {
+        $todoList[$i] = $todoList[$i + 1];
+    }
+
+    // logic menghapus data yg paling terakhir
+    unset($todoList[sizeof($todoList)]);
+
+    return true;
 }
 
 ?>
