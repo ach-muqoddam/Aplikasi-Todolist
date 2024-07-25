89ff0bd171e39eb33745f43415937dd56e2702ab
Ach Muqoddam <ach.adam04@gmail.com>
2024-07-25 16:44:13 +0700

    test view menambah todolist

--- Test/TestViewAddTodolist.php
+++ Test/TestViewAddTodolist.php
@@ -1,11 +1,7 @@
 <?php
 
-require_once "./Model/TodoList.php";
-
 require_once "./BusinessLogic/addTodoList.php";
 require_once "./BusinessLogic/showTodoList.php";
-
-require_once "./Helper/Input.php";
 
 require_once "./View/ViewAddTodoList.php";
 
