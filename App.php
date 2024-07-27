fce5a292e6cf4f2c49eb44550df841233de0e9cf
adam <ach.adam04@gmail.com>
2024-07-27 11:27:32 +0700

    test seluruh aplikasi

--- App.php
+++ App.php
@@ -1,13 +1,13 @@
 <?php 
 
-require_once "Model/Todolist.php";
+require_once __DIR__ . "/Helper/Input.php";
 
-require_once "BusinessLogic/showTodoList.php";
-require_once "BusinessLogic/addTodoList.php";
-require_once "BusinessLogic/removeTodoList.php";
+require_once __DIR__ . "/BusinessLogic/addTodoList.php";
+require_once __DIR__ . "/BusinessLogic/showTodoList.php";
+require_once __DIR__ . "/BusinessLogic/removeTodoList.php";
 
-require_once "View/ViewShowTodoList.php";
-require_once "View/ViewAddShowTodoList.php";
-require_once "View/ViewRemoveTodoList.php";
+require_once __DIR__ . "/View/ViewAddTodoList.php";
+require_once __DIR__ . "/View/ViewShowTodoList.php";
+require_once __DIR__ . "/View/ViewRemoveTodoList.php";
 
-echo "Aplikasi Todolist" . PHP_EOL;
+ViewShowTodoList();
